840c6a373625151e60129d8ec0bef1a4923eafd9
Abdul Majid <abmajidweb@gmail.com>
2020-10-21 14:06:40 +0600

    password reset

--- app/Admin.php
+++ app/Admin.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Notifications\Admin\ResetPassword;
 
 class Admin extends Authenticatable
 {
@@ -36,4 +37,15 @@ class Admin extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Send the password reset notification.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPassword($token));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,10 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::namespace('Admin')->prefix('admin')->group(function() {
     Route::get('login', 'LoginController@showLoginForm')->name('admin.login');
     Route::post('login', 'LoginController@login')->name('admin.login');
+    Route::get('password/reset', 'Passwords\ForgetPasswordController@showLinkRequestForm')->name('admin.password.request');
+    Route::post('password/email', 'Passwords\ForgetPasswordController@SendResetLinkEmail')->name('admin.password.email');
+    Route::get('password/reset/{token}', 'Passwords\ResetPasswordController@showResetForm')->name('admin.password.reset');
+    Route::post('password/reset', 'Passwords\ResetPasswordController@reset')->name('admin.password.update');
 
     Route::middleware('auth:admin')->group(function() {
         Route::post('logout', 'LoginController@logout')->name('admin.logout');
